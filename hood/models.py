0eeaa895cb1c638dbc0be7a23c8e041f58f57a76
happiness <happinesskawinzi@gmail.com>
2019-10-27 09:37:20 +0300

    creating neighborhood class and its properties

--- hood/models.py
+++ hood/models.py
@@ -3,10 +3,18 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Neighbourhood(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=30)
     user_id = models.AutoField()
     hood_id = models.IntegerField()
     email = models.TextField()
     date = models.DateField()
     location = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=70)
+    location = models.ManyToManyField(User)
+    occupants_count = models.IntegerField()
+
+
