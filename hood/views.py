3be25d579c326ca03b238136ac3978560e4c6037
happiness <happinesskawinzi@gmail.com>
2019-10-27 15:05:25 +0300

    creating an index view function

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import  login_required
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+    return render(request, 'index.html', {'posts': post, 'form': form})
+
+
